3fd02c0709956db41c6bec581414fd762d33b549
tandina <touretmc@protonmail.com>
2023-03-05 20:32:24 +0100

    correction du message d'erreur sur les id

--- src/containers/site/Site.jsx
+++ src/containers/site/Site.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import {Home,House,Error404,About} from '../index'
 import {Navbar} from '../../components/index'
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, useParams} from 'react-router-dom';
+import liste from '../../datas/kasadata.json';
 
+function HouseIdCheck() {
+  const { id } = useParams()
+  let houseId = liste.find(obj => obj.id=== id)
+
+  if (!houseId) {
+    return <Error404 />;
+  }
+  return <House element={<House/>} />;
+}
 
   class Site extends Component {
     render() {
@@ -13,7 +23,7 @@ import {Routes,Route} from 'react-router-dom';
                     <Routes>
                         <Route path="/" element={<Home/>} />
                         <Route path="/about" element={<About/>}/>
-                        <Route path="/house/:id" element={<House/>}/>
+                        <Route path="/house/:id" element={<HouseIdCheck/>}/>
                         <Route path="*" element={<Error404 />} />
                     </Routes>
                 </div> 
